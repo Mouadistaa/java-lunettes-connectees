132838316107523cb2b8da939fb653e09165b8da
theokachlergit <theokachler@gmail.com>
2026-05-06 13:38:07 +0200

    mise à jour inscription a l'évènement mqtt orders + désérialisation

--- lunettes-serveur/src/main/java/fr/miage/nancy/lunettes/serveur/ServeurMain.java
+++ lunettes-serveur/src/main/java/fr/miage/nancy/lunettes/serveur/ServeurMain.java
@@ -1,11 +1,16 @@
 package fr.miage.nancy.lunettes.serveur;
 
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
 import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.UUID;
+import fr.miage.nancy.lunettes.events.Deserializer;
+import fr.miage.nancy.lunettes.events.TypeLunette;
+
 
 public class ServeurMain {
     public static void main(String[] args) {
@@ -42,6 +47,28 @@ public class ServeurMain {
                         System.err.println("Erreur de connexion au broker MQTT : " + throwable.getMessage());
                     } else {
                         System.out.println("✅ Connecté avec succès au broker Mosquitto !");
+                        // 4. Écouter sur orders/+
+                        client.subscribeWith()
+                        .topicFilter("orders/+")
+                        .callback(publish -> {
+                            byte[] payloadBytes = publish.getPayloadAsBytes();
+                            String topic = publish.getTopic().toString();
+                            try {
+                                Map<TypeLunette, Integer> commande = Deserializer.deserializeCommandeLignes(payloadBytes);
+                                System.out.println("📦 Nouvelle commande reçue sur [" + topic + "] : " + commande);
+                                Deserializer.deserializeText(payloadBytes);
+                            } catch (Exception e) {
+                                System.err.println("❌ Impossible de désérialiser la commande : " + e.getMessage());
+                            }
+                        })
+                        .send()
+                        .whenComplete((subAck, subThrowable) -> {
+                            if (subThrowable != null) {
+                                System.err.println("Erreur de souscription : " + subThrowable.getMessage());
+                            } else {
+                                System.out.println("📡 En écoute sur le topic 'orders/+'...");
+                            }
+                        });
                     }
                 });
 
